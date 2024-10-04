d35ba5fa54d1d04beaa8530a0aafbdba678496ad
Cheezcuits <isaiah.sangilan@Gmail.com>
2024-10-04 18:28:53 +0800

    Update HiddenContent.js

--- HiddenContent.js
+++ HiddenContent.js
@@ -1,59 +1,77 @@
-const tabContentIds = ['All', 'Manga', 'Manhwa', 'AboutPage'];
+const tabContentIds = ['All', 'Manga', 'Manhwa', 'AboutPage', 'UpdateLog'];
 
 function hideAllContents() {
     tabContentIds.forEach(id => {
         const element = document.getElementById(id);
-        if (!element.classList.contains('hidden')) {
+        if (element && !element.classList.contains('hidden')) {
             element.classList.remove('slide-up');
             element.classList.add('hidden');
         }
     });
 }
 
+// Click event listener for each tab
 document.getElementById('tabs-1').addEventListener('click', function () {
     hideAllContents(); // Hide all contents first
     const element = document.getElementById('All');
-
-    element.classList.remove('hidden');
-    element.classList.add('slide-up');
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    if (element) {
+        element.classList.remove('hidden');
+        element.classList.add('slide-up');
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
 });
 
 document.getElementById('tabs-2').addEventListener('click', function () {
-    hideAllContents(); // Hide all contents first
+    hideAllContents();
     const element = document.getElementById('Manga');
-
-    element.classList.remove('hidden');
-    element.classList.add('slide-up');
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    if (element) {
+        element.classList.remove('hidden');
+        element.classList.add('slide-up');
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
 });
 
 document.getElementById('tabs-3').addEventListener('click', function () {
-    hideAllContents(); // Hide all contents first
+    hideAllContents();
     const element = document.getElementById('Manhwa');
-
-    element.classList.remove('hidden');
-    element.classList.add('slide-up');
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+    if (element) {
+        element.classList.remove('hidden');
+        element.classList.add('slide-up');
+        element.scrollIntoView({
+            behavior: '',
+            block: 'start'
+        });
+    }
 });
 
 document.getElementById('tabs-4').addEventListener('click', function () {
-    hideAllContents(); // Hide all contents first
+    hideAllContents();
     const element = document.getElementById('AboutPage');
+    if (element) {
+        element.classList.remove('hidden');
+        element.classList.add('slide-up');
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
+});
 
-    element.classList.remove('hidden');
-    element.classList.add('slide-up');
-    element.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
+document.getElementById('tabs-5').addEventListener('click', function () {
+    hideAllContents();
+    const element = document.getElementById('UpdateLog'); 
+    if (element) {
+        element.classList.remove('hidden');
+        element.classList.add('slide-up');
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    }
 });

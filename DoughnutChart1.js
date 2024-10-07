4a64ab6f66a2fe9111430e235b1d7ac338a45f5c
Cheezcuits <isaiah.sangilan@Gmail.com>
2024-10-07 12:56:59 +0800

    Update DoughnutChart1.js

--- DoughnutChart1.js
+++ DoughnutChart1.js
@@ -5,7 +5,7 @@ const DoughnutChart1 = new Chart(ctx, {
         labels: ['Manga','Manhwa'],
         datasets: [{
             label: '# of Mangas',
-            data: [19, 5],
+            data: [22, 9],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',

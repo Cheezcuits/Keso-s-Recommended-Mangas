e42f519616d21e9d55fb2372b10b580450a3a826
Cheezcuits <isaiah.sangilan@Gmail.com>
2024-10-14 18:01:31 +0800

    Update DoughnutChart2.js

--- DoughnutChart2.js
+++ DoughnutChart2.js
@@ -5,7 +5,7 @@ const DoughnutChart2 = new Chart(ctx2, {
         labels: ['Completed','Ongoing','Hiatus',],
         datasets: [{
             label: 'Status of Mangas',
-            data: [16, 21, 3,],
+            data: [17, 23, 3,],
             backgroundColor: [
                 'rgba(54, 162, 235, 0.2)', //blue (2)
                 'rgba(75, 192, 192, 0.2)', //green (4)

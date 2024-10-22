4ff2c68407cd0972b690cc0345dd25a33bd58a10
Cheezcuits <isaiah.sangilan@Gmail.com>
2024-10-22 19:01:21 +0800

    Update source.js

--- source.js
+++ source.js
@@ -64,3 +64,19 @@ document.addEventListener('mousemove', function (e) {
 document.addEventListener('mouseup', function () {
     isDragging = false;
 });
+
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
